12f1485951b307dd8e53c8bcd12d1e8297594b32
ErnieB20 <ernie312us@gmail.com>
2020-11-25 21:11:37 -0600

    updated UtilityPractice

--- src/test/java/day3/UtilityPractice.java
+++ src/test/java/day3/UtilityPractice.java
@@ -3,7 +3,9 @@ package day3;
 import utility.DB_Utility;
 
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.*;
 
 public class UtilityPractice {
 
@@ -39,10 +41,45 @@ public class UtilityPractice {
         System.out.println("Get Cell value at row 5 col JOB_TITLE "
                 + DB_Utility.getColumnDataAtRow(2, "JOB_TITLE") );
 
-        System.out.println("3rd column value "+DB_Utility.getColumnDataAsList(3));
-        System.out.println("JOB_TITLE column value "+ DB_Utility.getColumnDataAsList("JOB_TITLE"));
+        System.out.println("3rd column value " + DB_Utility.getColumnDataAsList(3) );
+        System.out.println("JOB_TITLE column value " + DB_Utility.getColumnDataAsList("JOB_TITLE") );
+
+        System.out.println("--------\n");
+//        DB_Utility.displayAllData();
+
+        Map<String,String>  row1Map = new LinkedHashMap<>() ; //new HashMap<>() ;
+        //JOB_ID, JOB_TITLE, MIN_SALARY, MAX_SALARY
+        //AC_ACCOUNT	Public Accountant	4200	9000
+        row1Map.put("JOB_ID", "AC_ACCOUNT");
+        row1Map.put("JOB_TITLE", "Public Accountant");
+        row1Map.put("MIN_SALARY", "4200");
+        row1Map.put("MAX_SALARY", "9000");
+
+        // now do above programmatically
+        // create row 1 map like above programmatically
+//        System.out.println("row1Map = " + row1Map);
+
+        System.out.println("first row rowMap = " + DB_Utility.getRowMap(1)  );
+
+        // if one row can be represented as one map object
+        // what data structure is good to store 19 rows of data
+        // List of Map
+
+        // Get 2nd row and 4th row and save it into list of map as practice
+        Map<String, String> row2Map = DB_Utility.getRowMap(2);
+        Map<String, String> row4Map = DB_Utility.getRowMap(4);
+
+        List<Map<String, String> > rowMapList = new ArrayList<>();
+        rowMapList.add(row2Map);
+        rowMapList.add(row4Map);
+
+        System.out.println(rowMapList);
+
 
         DB_Utility.destroy();
+
     }
 
 }
+
+
