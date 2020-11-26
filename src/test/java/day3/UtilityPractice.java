bbb6098129f40ffc4a08f9b5072e120fd708ecca
ErnieB20 <ernie312us@gmail.com>
2020-11-25 20:00:54 -0600

    Added UtilityPractice

--- src/test/java/day3/UtilityPractice.java
+++ src/test/java/day3/UtilityPractice.java
@@ -9,26 +9,26 @@ public class UtilityPractice {
 
     public static void main(String[] args) throws SQLException {
 
-        // test out all the methods you have created so far fro practice
+
+        // test out all the methods you have created so far for practice
         DB_Utility.createConnection();
 
         ResultSet jobRS = DB_Utility.runQuery("SELECT * FROM JOBS");
 
-        /*
-        * Get the row count of the ResultSet
-        * move the pointer to the last row and get the row number
+        /**
+         * Get the row count of the ResultSet
+         * move the pointer to the last row and get the row number
          */
 
-        jobRS.last();
-        int rowCount = DB_Utility.getRowCount(); // getRow is getting the current row number
-        System.out.println("rowCount + " + rowCount);
+        int rowCount =  DB_Utility.getRowCount() ;
+        System.out.println("rowCount = " + rowCount);
 
-        int colCount = DB_Utility.getColumnCount();
+        int colCount = DB_Utility.getColumnCount() ;
         System.out.println("colCount = " + colCount);
 
-        System.out.println("All column names " + DB_Utility.getColumnNames());
+        System.out.println("All Column names " +  DB_Utility.getColumnNames()  );
 
-        System.out.println("Row data at row 3 " + DB_Utility.getRowDataAsList(3));
+        System.out.println("Row data at row 3 " + DB_Utility.getRowDataAsList(3)  );
 
         System.out.println("Get Cell value at row 2 col 4 "
                 + DB_Utility.getColumnDataAtRow(2, 4) );
