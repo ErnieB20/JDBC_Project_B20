6b797cda22632126ae9429251ed37e91effb7432
ErnieB20 <ernie312us@gmail.com>
2020-11-28 11:38:43 -0600

    Created tests

--- src/test/java/day4/DB_Test.java
+++ src/test/java/day4/DB_Test.java
@@ -1,6 +1,7 @@
 package day4;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import utility.DB_Utility;
@@ -18,11 +19,18 @@ public class DB_Test {
     public void testEmployeeCount(){
         // run a query SELECT * FROM EMPLOYEES
         // assert that the employees count is 107
+        DB_Utility.runQuery("SELECT * FROM EMPLOYEES");
+        Assertions.assertEquals(107, DB_Utility.getRowCount() );
 
     }
     // Open another test
     // run query : SELECT * FROM REGIONS
     // assert -- 3rd row second column is Asia
+    @Test
+    public void test3rdRowSecondColumn(){
+        DB_Utility.runQuery("SELECT * FROM REGIONS");
+        Assertions.assertEquals("Asia", DB_Utility.getColumnDataAtRow(3, 2));
+    }
 
 
     @AfterAll
